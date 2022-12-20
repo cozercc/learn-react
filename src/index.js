3d9b30e1beb4d47c91e4cb8fa4ace0040132a963
cozer <cozercn@gmail.com>
2022-12-20 16:18:36 +0900

    send id to react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-function NumberList(props) {
-    const numbers = props.numbers;
-    const listItems = numbers.map((number) =>
-        <li key={number.toString()}>{number}</li>
+function Blog(props) {
+    const sidebar = (
+        <ul>
+            {props.posts.map((post) =>
+                <li key={post.id}>
+                    {post.title}
+                </li>
+            )}
+        </ul>
+    );
+
+    const content = props.posts.map((post) =>
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+        </div>
     );
 
     return (
-        <ul>{listItems}</ul>
-    )
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    );
 }
-const numbers = [1, 2, 3, 4, 5];
+const posts = [
+    { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
+    { id: 2, title: 'Installation', content: 'You can install React from npm.' }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<NumberList numbers={numbers} />);
+root.render(<Blog posts={posts} />)
