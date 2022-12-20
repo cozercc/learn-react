13592d5812153cc0d8b327e19353e53e5aa4ac09
cozer <cozercn@gmail.com>
2022-12-20 16:04:39 +0900

    list and key

--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-function WarningBanner(props) {
-    if (!props.warn) {
-        return null;
-    }
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <li key={number.toString()}>{number}</li>
+    );
 
     return (
-        <div className="warning">
-            Warning!
-        </div>
-    );
+        <ul>{listItems}</ul>
+    )
 }
-
-class Page extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { showWarning: true };
-    }
-
-    handleToggleClick = () => {
-        this.setState(state => ({
-            showWarning: !state.showWarning
-        }));
-    }
-
-    render() {
-        return (
-            <div>
-                <WarningBanner warn={this.state.showWarning} />
-                <button onClick={this.handleToggleClick}>
-                    {this.state.showWarning ? 'Hide' : 'Show'}
-                </button>
-            </div>
-        )
-    }
-}
+const numbers = [1, 2, 3, 4, 5];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Page />);
+root.render(<NumberList numbers={numbers} />);
