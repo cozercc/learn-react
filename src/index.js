da97085d6edf5ff3411996341c91b37f77d33aa6
cozercc <cozercn@gmail.com>
2023-01-18 17:38:01 +0900

    React哲学确定 state 放置的位置

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-
 class ProductCategoryRow extends React.Component {
     render() {
         const category = this.props.category;
@@ -36,10 +35,19 @@ class ProductRow extends React.Component {
 
 class ProductTable extends React.Component {
     render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+
         const rows = [];
         let lastCategory = null;
 
         this.props.products.forEach((product) => {
+            if (product.name.indexOf(filterText) === -1) {
+                return;
+            }
+            if (inStockOnly && !product.stocked) {
+                return;
+            }
             if (product.category !== lastCategory) {
                 rows.push(
                     <ProductCategoryRow
@@ -50,7 +58,8 @@ class ProductTable extends React.Component {
             rows.push(
                 <ProductRow
                     product={product}
-                    key={product.name} />
+                    key={product.name}
+                />
             );
             lastCategory = product.category;
         });
@@ -71,11 +80,19 @@ class ProductTable extends React.Component {
 
 class SearchBar extends React.Component {
     render() {
+        const filterText = this.props.filterText;
+        const inStockOnly = this.props.inStockOnly;
+
         return (
             <form>
-                <input type="text" placeholder="Search..." />
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={filterText} />
                 <p>
-                    <input type="checkbox" />
+                    <input
+                        type="checkbox"
+                        checked={inStockOnly} />
                     {' '}
                     Only show products in stock
                 </p>
@@ -85,11 +102,26 @@ class SearchBar extends React.Component {
 }
 
 class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        };
+    }
+
     render() {
         return (
             <div>
-                <SearchBar />
-                <ProductTable products={this.props.products} />
+                <SearchBar
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
+                <ProductTable
+                    products={this.props.products}
+                    filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                />
             </div>
         );
     }
@@ -107,5 +139,3 @@ const PRODUCTS = [
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<FilterableProductTable products={PRODUCTS} />);
-
-
